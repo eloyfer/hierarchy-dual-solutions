281105c33561b6c59d17577052e486c9ee11e2e4
eloyfer <loyfer.e@gmail.com>
2023-06-07 15:36:59 +0300

    Update collect_experiment_results.py

--- collect_experiment_results.py
+++ collect_experiment_results.py
@@ -2,13 +2,14 @@ import os
 import json
 import fire
 import glob
+from tqdm import tqdm
 import pandas as pd
 
 def collect_results(outdir):
   result_files = glob.glob(os.path.join(outdir, 'result_n*.json'))
   results = []
   required_keys = ['n','d','ell','m','sol_value']
-  for rf in result_files:
+  for rf in tqdm(result_files):
     with open(rf,'r') as fid:
       res = json.loads(fid.read())
     assert set(required_keys) <= set(res.keys())
